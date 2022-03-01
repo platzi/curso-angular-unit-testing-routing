045b81a3c24b735e7fe2175843050254e0537d69
Nicolas Molina Monroy <hi@nicobytes.com>
2022-03-01 15:34:18 -0400

    :white_check_mark: Test route with id

--- src/app/products/components/product-detail/product-detail.component.spec.ts
+++ src/app/products/components/product-detail/product-detail.component.spec.ts
@@ -1,14 +1,31 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+import { ActivatedRouteStub, mockObservable } from '../../../../testing';
+import { ProductsService } from '../../../services/product.service';
+import { generateOneProduct } from '../../../models/product.mock';
 
 import { ProductDetailComponent } from './product-detail.component';
 
-xdescribe('ProductDetailComponent', () => {
+fdescribe('ProductDetailComponent', () => {
   let component: ProductDetailComponent;
   let fixture: ComponentFixture<ProductDetailComponent>;
+  let route: ActivatedRouteStub;
+  let productsService: jasmine.SpyObj<ProductsService>;
+  let location: jasmine.SpyObj<Location>;
 
   beforeEach(async () => {
+    const routeStub = new ActivatedRouteStub();
+    const productServiceSpy = jasmine.createSpyObj('ProductsService', ['getOne']);
+    const locationSpy = jasmine.createSpyObj('Location', ['back']);
+
     await TestBed.configureTestingModule({
-      declarations: [ ProductDetailComponent ]
+      declarations: [ ProductDetailComponent ],
+      providers: [
+        { provide:  ActivatedRoute, useValue: routeStub },
+        { provide:  ProductsService, useValue: productServiceSpy },
+        { provide:  Location, useValue: locationSpy },
+      ]
     })
     .compileComponents();
   });
@@ -16,7 +33,21 @@ xdescribe('ProductDetailComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ProductDetailComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    route = TestBed.inject(ActivatedRoute) as unknown as ActivatedRouteStub;
+    productsService = TestBed.inject(ProductsService) as jasmine.SpyObj<ProductsService>;
+    location = TestBed.inject(Location) as jasmine.SpyObj<Location>;
+
+    const productId = '1';
+    route.setParamMap({ id: productId });
+
+    const productMock = {
+      ...generateOneProduct(),
+      id: productId,
+    }
+
+    productsService.getOne.and.returnValue(mockObservable(productMock));
+
+    fixture.detectChanges(); //ngOnInit
   });
 
   it('should create', () => {
--- src/testing/index.ts
+++ src/testing/index.ts
@@ -2,3 +2,4 @@ export * from './async-data';
 export * from './finders';
 export * from './click';
 export * from './forms';
+export * from './activated-route-stub';
