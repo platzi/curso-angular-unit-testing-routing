c9b53068fc8d4699d0effc051ec8a01d70652216
Nicolas Molina Monroy <hi@nicobytes.com>
2022-03-01 13:55:01 -0400

    :construction: Product detail

--- src/app/products/components/product-detail/product-detail.component.ts
+++ src/app/products/components/product-detail/product-detail.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+import { switchMap } from 'rxjs/operators';
+
+import { Product } from '../../../models/product.model'
+import { ProductsService } from '../../../services/product.service';
 
 @Component({
   selector: 'app-product-detail',
@@ -7,9 +13,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductDetailComponent implements OnInit {
 
-  constructor() { }
+  product: Product | null = null;
+
+  constructor(
+    private route: ActivatedRoute,
+    private productsService: ProductsService,
+    private location: Location
+  ) { }
 
   ngOnInit(): void {
+    this.route.paramMap
+      .subscribe((params) => {
+        const productId = params.get('id');
+        if (productId) {
+          this.getProductDetail(productId);
+        } else {
+          this.goToBack();
+        }
+      });
+  }
+
+  private getProductDetail(productId: string) {
+    this.productsService.getOne(productId)
+    .subscribe({
+      next: (product) => {
+        this.product = product;
+      },
+      error: () => {
+        this.goToBack();
+      }
+    })
+  }
+
+  goToBack() {
+    this.location.back();
   }
 
 }
